9a25b20774cc4c4dff4da51a796147abd06ace8d
Angélica Martins <marttinsangelica@gmail.com>
2022-11-07 20:58:29 -0300

    Feature: Add TypeScript

--- js/main.ts
+++ js/main.ts
@@ -1,12 +1,12 @@
-const timer = document.querySelector(".timer");
-const start = document.querySelector(".start");
-const pause = document.querySelector(".pause");
-const clear = document.querySelector(".clear");
+const timer = document.querySelector(".timer") as HTMLElement;
+const start = document.querySelector(".start") as HTMLElement;
+const pause = document.querySelector(".pause") as HTMLElement;
+const clear = document.querySelector(".clear") as HTMLElement;
 
-let intervalId;
+let intervalId: number;
 let second = 0;
 
-const getDate = () => {
+export const getDate = () => {
   const time = new Date(0, 0, 0, 0, 0, second);
   second++;
 
@@ -15,7 +15,7 @@ const getDate = () => {
 
 const startTimer = () => {
   clearInterval(intervalId);
-  timer.classList.remove("pausedTimer");
+  timer?.classList.remove("pausedTimer");
   
   intervalId = setInterval(function () {
     timer.innerText = getDate();
